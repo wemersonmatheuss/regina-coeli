4ab22d7fe447fe0e42137aa31799395feec4d7c1
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-07-31 17:36:24 -0300

    feat: criar sessao de cta

---
+++ src/components/Cta/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.css"
+
+import img from "../../assets/banner3.png"
+import { Button } from "../Button"
+
+export function Cta() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.image}>
+                <img src={img} alt="" />
+            </div>
+
+            <div className={styles.description}>
+                <h2>Matrículas 2026</h2>
+                <p>No Colégio Regina Coeli, tornamos o processo de matrícula mais prático e acolhedor para você e sua família. Sabemos o quanto esse momento é importante, por isso oferecemos a opção de agendar um horário com nossa equipe para um atendimento personalizado, com total atenção às suas dúvidas e necessidades. Durante a visita, você poderá conhecer nossa estrutura e entender melhor nossa proposta pedagógica. Venha fazer parte da família RC! </p>
+                <Button title="Agende uma visita"/>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import {} from "../main.css"
 
 import { Nav } from "../components/Nav"
-import Banner from "../components/Banner";
+import Banner from "../components/Banner"
+import { Cta } from "../components/Cta"
 
 export function Home() {
     return (
         <div>
             <Nav/>
             <Banner/>
+            <Cta/>
         </div>
     )
 }
