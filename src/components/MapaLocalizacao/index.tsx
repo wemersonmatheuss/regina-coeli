84a0a1f03610cc7ea5a48c8d992bd951c56f2f7a
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-08-04 09:34:29 -0300

    style: ajustar responsividade para dispositivos 390px

--- src/components/MapaLocalizacao/index.tsx
+++ src/components/MapaLocalizacao/index.tsx
@@ -1,6 +1,6 @@
 export function MapaLocalizacao() {
   return (
-    <div style={{ width: "380px", height: "auto", border: "none" }}>
+    <div style={{ width: "100%", height: "auto", border: "none" }}>
       <iframe
         title="Mapa Colégio Regina Coeli"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3947.1584156173014!2d-35.448074585300086!3d-7.874308994337384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a8a0dbb891da89%3A0x84f6c62d4e8188c!2sCol%C3%A9gio%20Regina%20Coeli!5e0!3m2!1spt-BR!2sbr!4v1712005883456!5m2!1spt-BR!2sbr"
